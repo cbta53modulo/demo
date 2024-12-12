d4c53c0c6883bd22161f2dfd0031aa221c9b31b0
bremont <jesusbremont@cbta53.edu.mx>
2024-12-12 09:56:44 -0700

    DDL LEO

--- views/empleado/_form.php
+++ views/empleado/_form.php
@@ -11,7 +11,7 @@ use app\models\Departamento;
 ?>
 
 <div class="empleado-form">
-    
+
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'nif')->textInput(['maxlength' => true]) ?>
@@ -22,11 +22,29 @@ use app\models\Departamento;
 
     <?= $form->field($model, 'apellido2')->textInput(['maxlength' => true]) ?>
 
+    <!-- Cargando valores directos -->
     <?= $form->field($model, 'codigo_departamento')->dropDownList(
         ArrayHelper::map(Departamento::find()->all(), 'codigo', 'nombre'),
         ['prompt' => 'Selecciona un Depa']
     )
     ?>
+
+    <!-- Otra forma usando consutal personalizada -->
+    <?php    
+
+    // Consulta personalizada para obtener los datos
+    $departamentos = Yii::$app->db->createCommand('SELECT codigo, nombre FROM departamento')->queryAll();
+
+    // Transformar los datos en un formato adecuado para el dropDownList
+    $departamentos = ArrayHelper::map($departamentos, 'codigo', 'nombre');
+
+    // Renderizar el campo con el dropDownList
+    echo $form->field($model, 'codigo_departamento')->dropDownList(
+        $departamentos,
+        ['prompt' => 'Selecciona un Depa']
+    );
+    ?>
+
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
